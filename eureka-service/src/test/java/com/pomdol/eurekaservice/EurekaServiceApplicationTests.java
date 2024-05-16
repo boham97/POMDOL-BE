a7a4f502d372626edd793fe6c80b4014b3d6a171
boham97 <kangbosung97@gmail.com>
2024-05-16 10:48:28 +0900

    init: eureka- service

---
+++ eureka-service/src/test/java/com/pomdol/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomdol.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
