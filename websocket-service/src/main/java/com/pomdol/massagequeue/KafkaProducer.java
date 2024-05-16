db783017a5dc3418e54c30e347da09f962ac4a31
boham97 <kangbosung97@gmail.com>
2024-05-16 13:37:16 +0900

    feat: kafka 연결

---
+++ websocket-service/src/main/java/com/pomdol/massagequeue/KafkaProducer.java
@@ -0,0 +1,43 @@
+package com.pomdol.massagequeue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pomdol.dto.ChatMessageDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public void messageSend(String topic, ChatMessageDto message){
+        String jsonInString = "";
+        try {
+            jsonInString = objectMapper.writeValueAsString(message);
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+        kafkaTemplate.send(topic, jsonInString);
+        log.info("kafka send: {} {}", topic, jsonInString);
+    }
+
+    public void inviteSend(ChatMessageDto message) {
+        String jsonInString = "";
+        try {
+            jsonInString = objectMapper.writeValueAsString(message);
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+        // redis 에서 유저가 있는 웹소켓 서버 찾는 로직
+        String topic = "service";
+        kafkaTemplate.send(topic, jsonInString);
+        log.info("kafka send: {} {}", topic, jsonInString);
+    }
+}
